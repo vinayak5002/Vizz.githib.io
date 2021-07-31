52abeb85807060400e7961f1df40af6f7fa2079b
vinayak5002 <82216732+vinayak5002@users.noreply.github.com>
2021-08-01 00:01:22 +0530

    Version-3.2 Load more fixed

--- Observer/API.js
+++ Observer/API.js
@@ -5,6 +5,7 @@ var temp;
 var more = 0, start = 5, end = 10;
 
 function change(cat){
+    window.scrollTo(0, 0);
     document.getElementById("cat").innerHTML = cat.innerHTML;
     if(cat.innerHTML === "Home"){
         temp = "all_news"
@@ -18,6 +19,7 @@ function change(cat){
 }
 
 function Load(){
+    more = 0; start = 5; end = 10;
     async function API() {
         let response = await fetch(base+type+postfix);
         let data = await response.json();
@@ -25,7 +27,20 @@ function Load(){
     }
     
     API().then( data => {
+        var news = document.getElementById("news");
+        news.innerHTML = "";
         for(var i=0; i<10; i++){
+            news.innerHTML += `<div class="card">
+                <div class="row">
+                    <div width="30%" class="column1">
+                        <img id="img${i+1}" src="" width="100%" margin-left="0px" alt="">
+                    </div>
+                    <div width="70%" class="column2">
+                        <h4 id="head${i+1}"></h4>
+                        <p id="txt${i+1}"></p>
+                    </div>
+                </div>
+            </div>`;
             document.getElementById(`head${i+1}`).innerHTML = data.articles[i].title;
             document.getElementById(`img${i+1}`).src = data.articles[i].image_url;
             document.getElementById(`txt${i+1}`).innerHTML = data.articles[i].description;
@@ -36,6 +51,7 @@ function Load(){
 function loadMore(){
     if(more < 3){
         more++;
+        console.log(more);
         start+=5;end+=5;
         async function API() {
             let response = await fetch(base+type+postfix);
